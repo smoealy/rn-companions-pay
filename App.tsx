fd7bb772c90c6387144e12c960641dc70059f6fd
smoealy <129239220+smoealy@users.noreply.github.com>
2025-08-11 18:47:22 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './src/screens/HomeScreen';
 import WalletScreen from './src/screens/WalletScreen';
 import SendMoneyScreen from './src/screens/SendMoneyScreen';
@@ -13,7 +14,9 @@ import RedeemScreen from './src/screens/RedeemScreen';
 import BuyTokensScreen from './src/screens/BuyTokensScreen';
 import TransactionHistoryScreen from './src/screens/TransactionHistoryScreen';
 import CardLoadScreen from './src/screens/CardLoadScreen';
+
 import { AppProvider } from './src/contexts/AppContext';
+import { AuthProvider } from './src/contexts/AuthContext';
 import { AppStackParamList } from './src/types';
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
@@ -21,22 +24,24 @@ const Stack = createNativeStackNavigator<AppStackParamList>();
 const App: React.FC = () => {
   return (
     <AppProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-          <Stack.Screen name="Wallet" component={WalletScreen} options={{ title: 'Wallet' }} />
-          <Stack.Screen name="SendMoney" component={SendMoneyScreen} options={{ title: 'Send Money' }} />
-          <Stack.Screen name="Card" component={CardScreen} options={{ title: 'Card' }} />
-          <Stack.Screen name="HajjGoals" component={HajjGoalsScreen} options={{ title: 'Hajj Goals' }} />
-          <Stack.Screen name="Rewards" component={RewardsScreen} options={{ title: 'Rewards' }} />
-          <Stack.Screen name="Settings" component={SettingsScreen} options={{ title: 'Settings' }} />
-          <Stack.Screen name="TopUp" component={TopUpScreen} options={{ title: 'Top Up' }} />
-          <Stack.Screen name="Redeem" component={RedeemScreen} options={{ title: 'Redeem' }} />
-          <Stack.Screen name="BuyTokens" component={BuyTokensScreen} options={{ title: 'Buy Tokens' }} />
-          <Stack.Screen name="TransactionHistory" component={TransactionHistoryScreen} options={{ title: 'Transaction History' }} />
-          <Stack.Screen name="CardLoad" component={CardLoadScreen} options={{ title: 'Card Load' }} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AuthProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Wallet" component={WalletScreen} />
+            <Stack.Screen name="SendMoney" component={SendMoneyScreen} />
+            <Stack.Screen name="Card" component={CardScreen} />
+            <Stack.Screen name="HajjGoals" component={HajjGoalsScreen} />
+            <Stack.Screen name="Rewards" component={RewardsScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
+            <Stack.Screen name="TopUp" component={TopUpScreen} />
+            <Stack.Screen name="Redeem" component={RedeemScreen} />
+            <Stack.Screen name="BuyTokens" component={BuyTokensScreen} />
+            <Stack.Screen name="TransactionHistory" component={TransactionHistoryScreen} />
+            <Stack.Screen name="CardLoad" component={CardLoadScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthProvider>
     </AppProvider>
   );
 };
