fa6fa4cbc876571567f48c3e68c58c086205d89a
smoealy <129239220+smoealy@users.noreply.github.com>
2025-08-11 19:31:17 -0400

    Update App.tsx
    feat(app): register web3 screens

--- App.tsx
+++ App.tsx
@@ -15,6 +15,9 @@ import RedeemScreen from './src/screens/RedeemScreen';
 import BuyTokensScreen from './src/screens/BuyTokensScreen';
 import CheckoutWebView from './src/screens/CheckoutWebView';
 import TransactionHistoryScreen from './src/screens/TransactionHistoryScreen';
+import Web3ConnectScreen from './src/screens/Web3ConnectScreen';
+import Web3TokenScreen from './src/screens/Web3TokenScreen';
+import Web3TransferScreen from './src/screens/Web3TransferScreen';
 import CardLoadScreen from './src/screens/CardLoadScreen';
 
 import { AppProvider } from './src/contexts/AppContext';
@@ -42,6 +45,9 @@ const App: React.FC = () => {
             <Stack.Screen name="BuyTokens" component={BuyTokensScreen} />
             <Stack.Screen name="Checkout" component={CheckoutWebView} />
             <Stack.Screen name="TransactionHistory" component={TransactionHistoryScreen} />
+            <Stack.Screen name="Web3Connect" component={Web3ConnectScreen} />
+            <Stack.Screen name="Web3Token" component={Web3TokenScreen} />
+            <Stack.Screen name="Web3Transfer" component={Web3TransferScreen} />
             <Stack.Screen name="CardLoad" component={CardLoadScreen} />
           </Stack.Navigator>
         </NavigationContainer>
