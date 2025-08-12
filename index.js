1214b82e85f24ec803a57f7783b919883fc195d9
smoealy <129239220+smoealy@users.noreply.github.com>
2025-08-11 21:11:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 import App from './App';
-
-// Register the main app component for Expo
 registerRootComponent(App);
-
-// Export default for Snack and other bundlers expecting a root component
 export default App;
