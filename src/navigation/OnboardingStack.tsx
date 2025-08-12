430c7076ec9db36b6d3fe0c0c48453fa46b6f61d
smoealy <129239220+smoealy@users.noreply.github.com>
2025-08-12 17:07:46 -0400

    Update OnboardingStack.tsx

--- src/navigation/OnboardingStack.tsx
+++ src/navigation/OnboardingStack.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import WelcomeScreen from '../screens/onboarding/WelcomeScreen';
 import SignInScreen from '../screens/onboarding/SignInScreen';
 import ChooseJourneyScreen from '../screens/onboarding/ChooseJourneyScreen';
 import GoalBasicsScreen from '../screens/onboarding/GoalBasicsScreen';
 import InviteFamilyScreen from '../screens/onboarding/InviteFamilyScreen';
-import Web3ToggleScreen from '../screens/onboarding/Web3ToggleScreen';
+import Web3ConnectScreen from '../screens/Web3ConnectScreen'; // <- final step
 import { OnboardingProvider } from '../contexts/OnboardingContext';
 
 export type OnboardingStackParamList = {
@@ -14,21 +15,22 @@ export type OnboardingStackParamList = {
   ChooseJourney: undefined;
   GoalBasics: undefined;
   InviteFamily: undefined;
-  Web3Toggle: undefined;
+  Web3Connect: undefined; // <- align route name with screen
 };
 
 const Stack = createStackNavigator<OnboardingStackParamList>();
 
-export const OnboardingStack: React.FC<{ onFinish: () => void }> = ({ onFinish }) => (
+const OnboardingStack: React.FC = () => (
   <OnboardingProvider>
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="ChooseJourney" component={ChooseJourneyScreen} />
       <Stack.Screen name="GoalBasics" component={GoalBasicsScreen} />
       <Stack.Screen name="InviteFamily" component={InviteFamilyScreen} />
-      <Stack.Screen name="Web3Toggle" component={Web3ToggleScreen} />
-      </Stack.Screen>
+      <Stack.Screen name="Web3Connect" component={Web3ConnectScreen} />
     </Stack.Navigator>
   </OnboardingProvider>
 );
+
+export default OnboardingStack;
